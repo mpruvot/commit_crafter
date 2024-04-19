e8a7cfb8df50decf650ba49e162e9a3a36b728a5
mariuspruvot <marius.pruvot@papernest.com>
2024-04-19 16:14:13 +0200

    chore: Update version to 0.1.2 in poetry configuration

--- commitcrafter/cli.py
+++ commitcrafter/cli.py
@@ -13,9 +13,12 @@ def generate():
     """Generate commit names based on the latest git diff."""
     diff = get_latest_diff(os.getcwd())
     if diff:
-        commit_names = generate_commit_names_using_chat(diff)
-        for name in commit_names:
-            typer.echo(name)
+        try:
+            commit_names = generate_commit_names_using_chat(diff)
+            for name in commit_names:
+                typer.echo(name)
+        except ValueError as e:
+            typer.echo(str(e))
     else:
         typer.echo("No diff found or diff is empty.")
 
