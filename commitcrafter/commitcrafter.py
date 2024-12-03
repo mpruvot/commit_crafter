344439ac8913c267774cc7fbb1e305c023df7d7b
mpruvot <marius.pruvot@outlook.fr>
2024-12-03 07:41:58 +0100

    ✨ feat: add support for claude ai client

--- commitcrafter/commitcrafter.py
+++ commitcrafter/commitcrafter.py
@@ -1,31 +1,42 @@
+# commitcrafter.py
 import os
-
 from git import Repo
 from git.exc import InvalidGitRepositoryError
 
 from commitcrafter.exceptions import EmptyDiffError
-from commitcrafter.gpt_integration import generate_commit_names_using_chat
+from commitcrafter.gpt_integration import generate_commit_names_using_chat as gptapi
+from commitcrafter.claude_integration import (
+    generate_commit_names_using_chat as claudeapi,
+)
 
 
 class CommitCrafter:
-    def __init__(self, path: str = os.getcwd(), compare_to: str = None):  # enum?
+    def __init__(self, path: str = os.getcwd(), compare_to: str = None):
         self.path = path
         self.compare_to = compare_to
 
-    def generate(self) -> list[str] | str:
+    def generate(self, client: str = "gpt") -> list[str] | str:
         """
         Generate commit names based on the latest git diff. Returns a list of commit names.
+        Args:
+            client (str): The AI client to use ("gpt" or "claude")
         Raises:
-            ValueError: If the OpenAI API key is not found in the environment variables.
+            ValueError: If the API key is not found in the environment variables.
             EmptyDiffError: If no changes are found in the latest commit.
         """
-
         diff = self._get_latest_diff()
 
         if not diff:
             raise EmptyDiffError
+
         try:
-            return generate_commit_names_using_chat(diff)
+            match client:
+                case "gpt":
+                    return gptapi(diff)
+                case "claude":
+                    return claudeapi(diff)
+                case _:
+                    raise ValueError(f"Unknown client: {client}")
         except ValueError:
             raise
 
